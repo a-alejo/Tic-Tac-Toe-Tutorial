bc00ff1a61bc0723110bfd165af2342482bdc4a0
Alfredo <76714792+aalejo92@users.noreply.github.com>
2021-05-29 15:14:10 -0400

    Update Tic-Tac-Toe Tutorial.py
    05-29-21 - reformatted code by adding proper spaces and blank lines, used f-Strings for print statement on play_game/handle_turn, changed tie (elif winner == None) to (elif winner is None) and corrected spelling errors

--- Tic-Tac-Toe Tutorial.py
+++ Tic-Tac-Toe Tutorial.py
@@ -1,45 +1,48 @@
-#Each comment explains before the code
-#Board display
+# Board display
 board = ['_', '_', '_',
          '_', '_', '_',
          '_', '_', '_']
 
-#Spaces/seperator in between moves
+
+# Spaces/separator in between moves
 def display_board():
     print('            Play Positions')
     print(board[0] + ' | ' + board[1] + ' | ' + board[2] + '     1 | 2 | 3')
     print(board[3] + ' | ' + board[4] + ' | ' + board[5] + '     4 | 5 | 6')
     print(board[6] + ' | ' + board[7] + ' | ' + board[8] + '     7 | 8 | 9')
-#Misc
+
+
+# Misc
 game_still_going = True
 winner = None
 current_player = 'X'
 
-#Tic-Tac-toe game play order flow(order of operations)
+
+# Tic-Tac-toe game play order flow(order of operations)
 def play_game():
     display_board()
     while game_still_going:
         handle_turn(current_player)
         check_if_game_over()
         flip_player()
-    if winner == 'X' or winner =='O':
-        print(winner + ' won!')
-    elif winner == None:
+    if winner == 'X' or winner == 'O':
+        print(f'{winner} won!')
+    elif winner is None:
         print('Tie!')
 
-#Turn basex2d placement code
-def handle_turn(player):
 
-    print(player +'\'s turn!')
+# Turn basex2d placement code
+def handle_turn(player):
+    print(f'{player}\'s turn!')
     position = input('Choose a position from 1 through 9: ')
     valid = False
     while not valid:
-        #Sets what input is allowed // while loop prevents from continuing unless given righ input
-        while position not in ['1','2','3','4','5','6','7','8','9']:
+        # Sets what input is allowed // while loop prevents from continuing unless given right input
+        while position not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
             position = input('Invalid input. Choose a position from 1 through 9: ')
-        #Subtracted 1 from position to make it easier for user to play since 0=1 for player
+        # Subtracted 1 from position to make it easier for user to play since 0=1 for player
         position = int(position) - 1
-        #This prevents from overwriting a spot that's already taken
+        # This prevents from overwriting a spot that's already taken
         if board[position] == "_":
             valid = True
         else:
@@ -47,22 +50,24 @@ def handle_turn(player):
     board[position] = player
     display_board()
 
-#Checks if game still in play
+
+# Checks if game still in play
 def check_if_game_over():
     check_for_winner()
     check_for_tie()
 
-#Checks if player Won
+
+# Checks if player Won
 def check_for_winner():
-    #Adding global links it together
+    # Adding global links it together
     global winner
     row_winner = check_rows()
-    coloumn_winner = check_columns()
+    column_winner = check_columns()
     diagonal_winner = check_diagonals()
     if row_winner:
         winner = row_winner
-    elif coloumn_winner:
-        winner = coloumn_winner
+    elif column_winner:
+        winner = column_winner
     elif diagonal_winner:
         winner = diagonal_winner
     else:
@@ -70,16 +75,16 @@ def check_for_winner():
     return
 
 
-#Checks if any blank space in the game
+# Checks if any blank space in the game
 def check_rows():
-    #Adding global links it together
+    # Adding global links it together
     global game_still_going
     row_1 = board[0] == board[1] == board[2] != '_'
     row_2 = board[3] == board[4] == board[5] != '_'
     row_3 = board[6] == board[7] == board[8] != '_'
     if row_1 or row_2 or row_3:
         game_still_going = False
-    #return the winner X or O
+    # return the winner X or O
     if row_1:
         return board[0]
     elif row_2:
@@ -90,14 +95,14 @@ def check_rows():
 
 
 def check_columns():
-    #Adding global links it together
+    # Adding global links it together
     global game_still_going
     column_1 = board[0] == board[3] == board[6] != '_'
     column_2 = board[1] == board[4] == board[7] != '_'
     column_3 = board[2] == board[5] == board[8] != '_'
     if column_1 or column_2 or column_3:
         game_still_going = False
-    #return the winner X or O
+    # return the winner X or O
     if column_1:
         return board[0]
     elif column_2:
@@ -106,8 +111,9 @@ def check_columns():
         return board[2]
     return
 
+
 def check_diagonals():
-    #Adding global links it together
+    # Adding global links it together
     global game_still_going
     diagonal_1 = board[0] == board[4] == board[8] != '_'
     diagonal_2 = board[6] == board[4] == board[2] != '_'
@@ -120,19 +126,21 @@ def check_diagonals():
         return board[6]
     return
 
-#Checks if player tie
+
+# Checks if player tie
 def check_for_tie():
-    #Adding global links it together
+    # Adding global links it together
     global game_still_going
     if '_' not in board:
         game_still_going = False
     return
 
-#Changes turn to other player
+
+# Changes turn to other player
 def flip_player():
-    #Adding global links it together
+    # Adding global links it together
     global current_player
-#Changes from X to O per turn
+    # Changes from X to O per turn
     if current_player == 'X':
         current_player = 'O'
     elif current_player == 'O':
